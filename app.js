e9f1cbac0852781d1abbcbba7dba66d7d49eff40
romelmahmud <romelmahmud@outlook.com>
2021-05-24 17:48:34 +0600

    complete

--- app.js
+++ app.js
@@ -1,2 +1,19 @@
 //using selectors inside the element
+
 // traversing the dom
+
+const Buttons = document.querySelectorAll(".question-btn");
+const questions = document.querySelectorAll(".question");
+
+Buttons.forEach((btn) => {
+  btn.addEventListener("click", () => {
+    const article = btn.parentElement.parentElement;
+
+    article.classList.toggle("show-text");
+    questions.forEach((ques) => {
+      if (ques.classList !== article.classList) {
+        ques.classList.remove("show-text");
+      }
+    });
+  });
+});
